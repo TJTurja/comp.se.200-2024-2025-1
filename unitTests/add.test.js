a89b36b28504c29d4efb14268b858bc1ccd31031
Tero Turja <tero.turja@tuni.fi>
2024-10-30 20:19:30 +0200

    update test to use import instead of require

--- unitTests/add.test.js
+++ unitTests/add.test.js
@@ -1,11 +1,11 @@
-const {expect} = require('chai');
-const {add} = require('../src/add');
+import {expect} from 'chai';
+import add from '../src/add.js';
 
 describe('Simple mocha test to verify ci pipeline functionality', ()=>{
     describe('add()', ()=>{
         it('1 + 2 should equal 3', ()=>{
             const res = add(1, 2);
-            expect(res).to.equal(5);
+            expect(res).to.equal(3);
         });
     });
 });
